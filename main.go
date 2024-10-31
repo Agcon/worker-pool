2d33dac6eb24f4b9f791a6d3331035cb249425b6
Agcon <agconagconovich@gmail.com>
2024-10-31 16:12:44 +0300

    second

--- main.go
+++ main.go
@@ -9,18 +9,21 @@ import (
 type Worker struct {
 	ID        int
 	Job       <-chan string
+	Done      chan struct{}
 	WaitGroup *sync.WaitGroup
 }
 
 type WorkerPool struct {
 	Job       chan string
 	WaitGroup *sync.WaitGroup
+	Workers   []*Worker
 }
 
 func NewWorker(id int, job <-chan string, wg *sync.WaitGroup) *Worker {
 	return &Worker{
 		ID:        id,
 		Job:       job,
+		Done:      make(chan struct{}),
 		WaitGroup: wg,
 	}
 }
@@ -28,9 +31,19 @@ func NewWorker(id int, job <-chan string, wg *sync.WaitGroup) *Worker {
 func (worker *Worker) Work() {
 	go func() {
 		defer worker.WaitGroup.Done()
-		for job := range worker.Job {
-			fmt.Printf("Воркер %d выполняет работу %s\n", worker.ID, job)
-			time.Sleep(time.Second)
+		for {
+			select {
+			case job, ok := <-worker.Job:
+				if !ok {
+					fmt.Printf("Воркер %d завершил свою работу\n", worker.ID)
+					return
+				}
+				fmt.Printf("Воркер %d выполняет %s\n", worker.ID, job)
+				time.Sleep(time.Second)
+			case <-worker.Done:
+				fmt.Printf("Воркер %d завершил свою работу\n", worker.ID)
+				return
+			}
 		}
 	}()
 }
@@ -39,6 +52,7 @@ func NewWorkerPool() *WorkerPool {
 	return &WorkerPool{
 		Job:       make(chan string, 10),
 		WaitGroup: &sync.WaitGroup{},
+		Workers:   []*Worker{},
 	}
 }
 
@@ -47,14 +61,27 @@ func (wp *WorkerPool) InitialWorkers(number int) {
 		wp.WaitGroup.Add(1)
 		worker := NewWorker(i, wp.Job, wp.WaitGroup)
 		worker.Work()
+		wp.Workers = append(wp.Workers, worker)
 	}
 }
 
 func (wp *WorkerPool) AddWorkers(number int) {
 	for i := 0; i < number; i++ {
 		wp.WaitGroup.Add(1)
-		worker := NewWorker(i+1+len(wp.Job), wp.Job, wp.WaitGroup)
+		worker := NewWorker(len(wp.Workers), wp.Job, wp.WaitGroup)
 		worker.Work()
+		wp.Workers = append(wp.Workers, worker)
+	}
+}
+
+func (wp *WorkerPool) RemoveWorkers(number int) {
+	if number > len(wp.Workers) {
+		number = len(wp.Workers)
+	}
+	for i := 0; i < number; i++ {
+		worker := wp.Workers[len(wp.Workers)-1]
+		close(worker.Done)
+		wp.Workers = wp.Workers[:len(wp.Workers)-1]
 	}
 }
 
@@ -65,8 +92,8 @@ func main() {
 	workerPool.InitialWorkers(initialNumberOfWorkers)
 	// Отправляем задачи в канал
 	go func() {
-		for i := 1; i <= 50; i++ {
-			workerPool.Job <- fmt.Sprintf("Выполнение работы номер %d", i)
+		for i := 1; i <= 80; i++ {
+			workerPool.Job <- fmt.Sprintf("работу номер %d", i)
 		}
 		close(workerPool.Job)
 	}()
@@ -75,6 +102,10 @@ func main() {
 
 	fmt.Println("Добавление 5 новых работников")
 	workerPool.AddWorkers(5)
+
+	time.Sleep(3 * time.Second)
+	fmt.Println("Удаление 3 работников")
+	workerPool.RemoveWorkers(3)
 	// Ждем завершения всех воркеров
 	workerPool.WaitGroup.Wait()
 	fmt.Println("Все работы завершены")
